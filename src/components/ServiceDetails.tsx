069ce3695e86359c3444a6055b9ed6e4983bd3d7
sizwekenny <sizwekenny36@gmail.com>
2025-07-31 15:45:55 +0200

    few updates

--- src/components/ServiceDetails.tsx
+++ src/components/ServiceDetails.tsx
@@ -225,7 +225,7 @@ const ServiceDetails: React.FC<ServiceDetailsProps> = ({ service, onBack }) => {
                 </div>
                 <div className="flex items-center">
                   <MapPin className="w-5 h-5 mr-3" />
-                  <span>Building 12, Ground Floor</span>
+                  <span>Building 5</span>
                 </div>
               </div>
             </div>
